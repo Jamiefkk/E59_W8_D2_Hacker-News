edea9a239a62b8b7d8c7ac9a0150257891e6d92a
Jamie <jamiefogartykk@gmail.com>
2022-08-09 15:21:24 +0100

    second commit

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -4,37 +4,42 @@ import NewsDetail from '../components/NewsDetail';
 
 const NewsContainer = () => {
 
-    const [newsStories, setNewsStories] = useState([]);
+    // const [newsStories, setNewsStories] = useState([]);
     const [selectedStory, setSelectedStory] = useState(null);
-    const selectedStoryJson = `https://hacker-news.firebaseio.com/v0/item/${selectedStory}.json`
-    const [selectedStoryApi, setSelectedStoryApi] = useState(null);
-
+    const [top20, setTop20] = useState([]);
 
     useEffect( () => {
-        fetch( 'https://hacker-news.firebaseio.com/v0/topstories.json' )
-            .then( res => res.json() )
-            .then( data => setNewsStories(data) )
-    }, [] );
-
-    useEffect( () => {
-        fetch( selectedStoryJson )
-            .then( res => res.json() )
-            .then( data => setSelectedStoryApi(data) )
-    }, [] )
+        fetch("https://hacker-news.firebaseio.com/v0/topstories.json").then(
+            (res => res.json())
+        ).then(
+            (data) =>  {
+            const topStories = data.slice(0, 20);
+            setTop20(topStories);
+            const promisedStories = topStories.map((topStory) => {
+                    return fetch(`https://hacker-news.firebaseio.com/v0/item/${topStory}.json`)
+                    .then((res) => (res.json()))
+                })
+            
+            console.log(promisedStories)
+            Promise.all(promisedStories).then((data) => {
+                setTop20(data);
+            })
+        } 
+        )
+    }, [])
 
     const handleSelectChange = ( story ) => {
         setSelectedStory( story );
-        setSelectedStoryApi(selectedStoryJson);
     }
 
     return (
         <div>
             <h1>News</h1>
-            <NewsSelect newsStories={ newsStories } handleSelectChange={ handleSelectChange } />
-            { selectedStoryApi ? <NewsDetail newsStory={ selectedStoryApi } /> : null }
+            <NewsSelect newsStories={ top20 } handleSelectChange={ handleSelectChange } />
+            { selectedStory ? <NewsDetail newsStory={ selectedStory } /> : null }
         </div>
     )
 }
 
 
-export default NewsContainer
+export default NewsContainer;
